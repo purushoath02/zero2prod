a95a970c7d9c12dd8c0e1763477ad109158487fc
purushoath02 <purushoath2002@gmail.com>
2024-08-18 01:04:34 +0530

    Axum tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::response::Html;
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let app = Router::new().route("/", get(|| health_check()));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app).await.unwrap();
+    Ok(())
+}
+
+async fn health_check() -> Html<&'static str> {
+    Html("<h1>Hello, world!</h1>")
 }
